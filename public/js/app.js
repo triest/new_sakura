d5a11f197368ba0f9e20454140b63b6979a65c8b
triest <you@example.com>
2021-01-12 17:52:35 +0300

    Fix Pusher_API_KEY

--- public/js/app.js
+++ public/js/app.js
@@ -62577,9 +62577,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "68cff25802da8182ecc1",
-  cluster: "eu",
-  forceTLS: true
+  key: "{{ env('PUSHER_APP_KEY') }}"
 });
 
 /***/ }),
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -35,7 +35,5 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
  window.Echo = new Echo({
      broadcaster: 'pusher',
-     key: process.env.MIX_PUSHER_APP_KEY,
-     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-     forceTLS: true
+     key:   "{{ env('PUSHER_APP_KEY') }}",
  });
