5dd7560a049a58e753761d94e98f5cbd9f7d9899
triest <you@example.com>
2021-01-24 16:50:42 +0300

    Delete .env

--- app/Http/Controllers/Lk/HomeController.php
+++ app/Http/Controllers/Lk/HomeController.php
@@ -14,6 +14,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Storage;
+use Imagick;
 
 
 class HomeController extends Controller
