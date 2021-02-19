18fb9fee42837f20067473836665dda9d5a3df83
triest <triest21@gmail.com>
2021-02-19 11:49:34 +0300

    add vue gallery

--- resources/js/components/anket/album.vue
+++ resources/js/components/anket/album.vue
@@ -1,168 +1,166 @@
 <template>
-  <div>
-    <div v-if="owner">
-      Загрузить фотографию
-      <input type="file" id="photo" name="photo" ref="galerayFileInput" v-on:change="handleFileUpload()">
-      <button type="button" class="btn btn-primary" v-on:click="submitFile()">Загрузить</button>
+    <div>
+        <img v-for="(image, i) in images"  height="400rem" :src="image" @click="index = i">
+        <vue-gallery-slideshow :images="images" :index="index" @close="index = null"></vue-gallery-slideshow>
     </div>
-    <div v-if="photos.length!=0">
-    <div class="col-lg-3 col-md-5 col-sm-6  justify-content-center col-xs-9 box-shadow" v-for="item in photos"
-         style="padding-left:60px; padding-right: 20px;margin: auto;">
-      <img width="250" height="250" :src="'/'+item.url"  class="photo" v-on:click="showPhoto(item)">
-      <span v-if="owner">
-        <button class="btn btn-danger" v-on:click="deletePhoto(item.id)">Удалить</button>
-      </span>
-    </div>
-    </div>
-    <span class="not_photo" v-else>
-       В альбоме нет фотографий
-    </span>
-    <div v-if="photos.length!=0">
-        <photo-modal :photo="photo_item" :photos="photos" v-if="showPhotoModal" @closePhotoModal="closeModal()" class="photoModal-component"></photo-modal>
-    </div>
-  </div>
 </template>
 
 <script>
 import photoModal from './photoModal';
+import VueGallerySlideshow from 'vue-gallery-slideshow';
+
 export default {
-  props: {
-    user_id: {
-      type: Number,
-      required: false
+    props: {
+        user_id: {
+            type: Number,
+            required: false
+        },
+        owner: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+        album_id: {
+            type: Number,
+            required: false
+        },
     },
-    owner: {
-      type: Boolean,
-      required: false,
-      default: false,
+    computed: {},
+    data() {
+        return {
+            images: [],
+            photos: null,
+            photos_array: [],
+            photo_item: null,
+            photo: null,
+            galerayFile: null,
+            showPhotoModal: false,
+            resizable: true,
+            adaptive: true,
+            draggable: true,
+            index: null,
+        };
     },
-    album_id: {
-      type: Number,
-      required: false
+    mounted() {
+        this.getPhotos();
+        this.$modal.show('example-modal')
     },
-  },
-  data() {
-    return {
-      photos: null,
-      photos_array:[],
-      photo_item:null,
-      photo: null,
-      galerayFile: null,
-      showPhotoModal:false,
-      resizable: true,
-      adaptive: true,
-      draggable: true
-    };
-  },
-  mounted() {
-    this.getPhotos();
-    this.$modal.show('example-modal')
-  },
-  components:{
-    photoModal
-  },
-  methods: {
-    getPhotos() {
-      let url = '/api/anket/' + this.user_id + '/album/' + this.album_id;
-      let that = this;
-      this.photos = [];
-      axios.get(url, {}).then(function (response) {
-        let data = null;
-        data = response.data;
-        that.photos = data.photos;
-      });
+    components: {
+        photoModal,
+        VueGallerySlideshow
     },
-    deletePhoto(id) {
-      let result = window.confirm("Удалить фотографию?");
-      if (!result) {
-        return;
-      }
-      let formData = new FormData();
-      formData.append('image_id', id);
-      let url = '/api/anket/' + this.user_id + '/albums/' + this.album_id + '/delete/' + id;
-      let that = this;
-      axios.delete(url,
-          formData,
-          {
-            headers: {
-              'Content-Type': 'multipart/form-data'
+    methods: {
+        getPhotos() {
+            let url = '/api/anket/' + this.user_id + '/album/' + this.album_id;
+            let that = this;
+            this.photos = [];
+            axios.get(url, {}).then(function (response) {
+                let data = null;
+                data = response.data;
+                that.photos = data.photos;
+                that.getImages();
+            });
+        },
+        getImages() {
+            let array = [];
+            for (let i = 0; i < this.photos.length; i++) {
+                console.log(this.photos[i].url)
+                this.images.push("/" + this.photos[i].url);
             }
-          }
-      ).then(function (response) {
-        let data = null;
+            console.log(this.images);
+            //return array;
+        },
+        deletePhoto(id) {
+            let result = window.confirm("Удалить фотографию?");
+            if (!result) {
+                return;
+            }
+            let formData = new FormData();
+            formData.append('image_id', id);
+            let url = '/api/anket/' + this.user_id + '/albums/' + this.album_id + '/delete/' + id;
+            let that = this;
+            axios.delete(url,
+                formData,
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+            ).then(function (response) {
+                let data = null;
 
-      })
-          .catch(function () {
-            Alert("Ошибка!")
-          });
-      this.getPhotos();
-    },
-    handleFileUpload() {
-      this.galerayFile = this.$refs.galerayFileInput.files[0];
-    },
-    submitFile() {
-      let formData = new FormData();
-      formData.append('image', this.galerayFile);
-      let url = '/api/anket/' + this.user_id + '/albums/' + this.album_id + '/upload/image';
-      let that = this;
-      axios.post(url,
-          formData,
-          {
-            headers: {
-              'Content-Type': 'multipart/form-data'
-            }
-          }
-      ).then(function (response) {
-        let data = null;
-        data = response.data;
-        that.photos.push(data.photo)
-        $("#photo")[0].value = "";
-      })
-          .catch(err => {
-            let message = "";
-            if (err.response.status === 422) {
-              message = Object.values(err.response.data.errors).join('<br>')
-            }
-            if (err.response.status === 500) {
-              message += "Ошибка. Обратитесь к администратору";
-            }
+            })
+                .catch(function () {
+                    Alert("Ошибка!")
+                });
+            this.getPhotos();
+        },
+        handleFileUpload() {
+            this.galerayFile = this.$refs.galerayFileInput.files[0];
+        },
+        submitFile() {
+            let formData = new FormData();
+            formData.append('image', this.galerayFile);
+            let url = '/api/anket/' + this.user_id + '/albums/' + this.album_id + '/upload/image';
+            let that = this;
+            axios.post(url,
+                formData,
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+            ).then(function (response) {
+                let data = null;
+                data = response.data;
+                that.photos.push(data.photo)
+                $("#photo")[0].value = "";
+            })
+                .catch(err => {
+                    let message = "";
+                    if (err.response.status === 422) {
+                        message = Object.values(err.response.data.errors).join('<br>')
+                    }
+                    if (err.response.status === 500) {
+                        message += "Ошибка. Обратитесь к администратору";
+                    }
 
-            alert(message)
-          })
-      //   this.getPhotos();
-    },
-    showPhoto(photo){
-        this.photo_item=photo;
-        this.photos_array=this.photos;
-        this.showPhotoModal=true;
-    },
-    closeModal(){
-        this.showPhotoModal=false;
+                    alert(message)
+                })
+            //   this.getPhotos();
+        },
+        showPhoto(photo) {
+            this.photo_item = photo;
+            this.photos_array = this.photos;
+            this.showPhotoModal = true;
+        },
+        closeModal() {
+            this.showPhotoModal = false;
+        }
+
     }
-
-  }
 }
 </script>
 
 <style scoped>
 .not_photo {
-  width: 25rem;
-  background-color: #eeeeee;
-  border: 1px solid transparent;
-  display:inline;
-  text-align: center;
-  position:absolute;
-  margin-left: 40%;
+    width: 25rem;
+    background-color: #eeeeee;
+    border: 1px solid transparent;
+    display: inline;
+    text-align: center;
+    position: absolute;
+    margin-left: 40%;
 }
 
-.photo{
-   cursor: pointer;
+.photo {
+    cursor: pointer;
 }
 
-.photoModal-component{
-  position: absolute;
-  margin-top: auto;
-  margin-left: auto;
+.photoModal-component {
+    position: absolute;
+    margin-top: auto;
+    margin-left: auto;
 }
 
 </style>
--- resources/js/components/anket/photoModal.vue
+++ resources/js/components/anket/photoModal.vue
@@ -26,123 +26,124 @@
 <script>
 
 
-    export default {
-        props: {
-            photo:{
-                type: Object,
-                required: false
-            },
-            photos:{
-                type:Array,
-                required: false,
-                default:[]
+export default {
+    props: {
+        photo: {
+            type: Object,
+            required: false
+        },
+        photos: {
+            type: Array,
+            required: false,
+            default: []
+        }
+    },
+    name: 'modal',
+    mounted() {
+        this.checkScroll()
+    },
+    computed: {
+        number: function () {
+            let index = this.arrayKeySearch(this.photos, this.photo.id);
+            return index + 1;
+        }
+    },
+    data() {
+        return {
+            MessageText: "",
+            message: "",
+            showPhotoModal: false,
+            scrollNext: false,
+            scrollPrevision: false,
+            index: null
+        }
+    },
+    methods: {
+        close() {
+            this.$emit('closePhotoModal')
+        },
+        arrayKeySearch(arr, val) {  //задает переменную по цвету
+            return arr.indexOf(this.photo);
+        },
+        checkScroll() {
+            let index = this.arrayKeySearch(this.photos, this.photo.id); //индек текущей фотки в массивк
+            //проверка на возможность скролинга назад
+            if (index === 0) {
+                this.scrollPrevision = false;
+            } else {
+                this.scrollPrevision = true;
+            }
+            //проверка на возможность скролинга вперед
+            if (this.photos.length - 1 > (index)) {
+                this.scrollNext = true;
+            } else {
+                this.scrollNext = false;
             }
         },
-        name: 'modal',
-        mounted() {
-              this.checkScroll()
+        scrollNextFunction() {
+            let index = this.arrayKeySearch(this.photos, this.photo); //индек текущей фотки в массивк
+            this.photo = this.photos[index + 1];
+            this.checkScroll()
         },
-        computed: {
-           number:function (){
-             let index=this.arrayKeySearch(this.photos,this.photo.id);
-             return index+1;
-           }
-        },
-        data() {
-            return {
-                MessageText: "",
-                message: "",
-                showPhotoModal:false,
-                scrollNext:false,
-                scrollPrevision:false,
-                index:null
-            }
-        },
-        methods: {
-            close() {
-                  this.$emit('closePhotoModal')
-            },
-            arrayKeySearch(arr, val) {  //задает переменную по цвету
-              return arr.indexOf(this.photo);
-            },
-            checkScroll(){
-                 let index=this.arrayKeySearch(this.photos,this.photo.id); //индек текущей фотки в массивк
-                  //проверка на возможность скролинга назад
-                 if(index===0){
-                   this.scrollPrevision=false;
-                 }else {
-                   this.scrollPrevision=true;
-                 }
-                  //проверка на возможность скролинга вперед
-                   if(this.photos.length-1 > (index)){
-                      this.scrollNext=true;
-                   }else {
-                      this.scrollNext=false;
-                   }
-            },
-            scrollNextFunction(){
-              let index=this.arrayKeySearch(this.photos,this.photo); //индек текущей фотки в массивк
-              this.photo=this.photos[index+1];
-              this.checkScroll()
-            },
-            scrollPrevisionFunction(){
-              let index=this.arrayKeySearch(this.photos,this.photo); //индек текущей фотки в массивк
-              this.photo=this.photos[index-1];
-              this.checkScroll()
-            }
-        },
-    };
+        scrollPrevisionFunction() {
+            let index = this.arrayKeySearch(this.photos, this.photo); //индек текущей фотки в массивк
+            this.photo = this.photos[index - 1];
+            this.checkScroll()
+        }
+    },
+};
 </script>
 
 <style scoped>
 
-    .modal-container {
-        width: auto;
-        height: auto;
-        overflow-x: hidden;
-        overflow-y: hidden;
-        /*height:300px;*/
-  /*      margin: 0px auto;
-        padding: 20px 30px;
-        background-color: #fff;
-        border-radius: 2px;
-        box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
-        transition: all .3s ease;
-        font-family: Helvetica, Arial, sans-serif;
-        overflow: hidden*/
-    }
+.modal-container {
+    width: auto;
+    height: auto;
+    overflow-x: hidden;
+    overflow-y: hidden;
+    /*height:300px;*/
+    /*      margin: 0px auto;
+          padding: 20px 30px;
+          background-color: #fff;
+          border-radius: 2px;
+          box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+          transition: all .3s ease;
+          font-family: Helvetica, Arial, sans-serif;
+          overflow: hidden*/
+}
 
-    .modal-header h3 {
-        margin-top: 0;
-        color: #42b983;
-    }
+.modal-header h3 {
+    margin-top: 0;
+    color: #42b983;
+}
 
-    .album-image{
-      width:auto;
-      height: auto;
-      max-height: 80ch;
-    }
+.album-image {
+    width: auto;
+    height: auto;
+    max-height: 80ch;
+}
 
-    /*
-     * The following styles are auto-applied to elements with
-     * transition="modal" when their visibility is toggled
-     * by Vue.js.
-     *
-     * You can easily play with the modal transition by editing
-     * these styles.
-     */
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
 
-    .modal-enter {
-        opacity: 0;
-    }
+.modal-enter {
+    opacity: 0;
+}
 
-    .modal-leave-active {
-        opacity: 0;
-    }
+.modal-leave-active {
+    opacity: 0;
+}
 
-    .album-image{
-        width: 80ch;
-    }
+.album-image {
+    width: 80ch;
+}
+
 /*
     .modal-enter .modal-container,
     .modal-leave-active .modal-container {
@@ -151,35 +152,35 @@
     }
 */
 
-    .newMessageModal {
-      position: fixed;
-      bottom: 0;
-      right: 0;
-    }
-
-.modal-container {
-  bottom: 0;
-  left: 0;
-  height: 80%;
-  width: 80%;
-  margin: auto;
-  position: absolute;
-  right: 0;
-  text-align: center;
-  top: 0;
+.newMessageModal {
+    position: fixed;
+    bottom: 0;
+    right: 0;
 }
 
-.avatar{
- height:  30%;
+.modal-container {
+    bottom: 0;
+    left: 0;
+    height: 80%;
+    width: 80%;
+    margin: auto;
+    position: absolute;
+    right: 0;
+    text-align: center;
+    top: 0;
+}
+
+.avatar {
+    height: 30%;
 }
 
 .container img {
-  max-width: 100%;
+    max-width: 100%;
 }
 
-.close_modal_button{
-   position: relative;
-   right: -60%;
+.close_modal_button {
+    position: relative;
+    right: -60%;
 }
 
 </style>
